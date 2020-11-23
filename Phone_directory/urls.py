82382b58bddc96dde2e7180c1da6c6b962c73483
sharg <jenya.sharunov@gmail.com>
2020-11-23 04:10:55 +0300

    delet drf yasg

--- Phone_directory/urls.py
+++ Phone_directory/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .yasg import urlpatterns as documentationUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-urlpatterns+=documentationUrls
